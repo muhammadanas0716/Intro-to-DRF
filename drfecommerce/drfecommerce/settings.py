d024c77359ca5b4b1d7fcfa5b245e5a1621eaace
Muhammad Anas <iam.muhammadanas0716@gmail.com>
2024-02-11 19:19:18 +0500

    product app

--- drfecommerce/drfecommerce/settings.py
+++ drfecommerce/drfecommerce/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    
+    # External apps
+    'rest_framework',
+    
+    # Internal apps
+    'drfecommerce.product',
 ]
 
 MIDDLEWARE = [
