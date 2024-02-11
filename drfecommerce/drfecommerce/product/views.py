3908860055b48a8bf92d5de59ff201b0800d79d0
Muhammad Anas <iam.muhammadanas0716@gmail.com>
2024-02-11 22:35:31 +0500

    Add CategoryViewSet for viewing all categories

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
 
-# Create your views here.
+
+from .models import Category
+from .serializers import CategorySerializer
+
+class CategoryViewSet(viewsets.ViewSet):
+  """
+  A simple viewset for viewing all categories
+  """
+  
+  queryset = Category.objects.all()
+  
+  def list(self, request):
+    serializer = CategorySerializer(self.queryset, many=True)
+    
+    return Response(serializer.data)
